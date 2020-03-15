dda1aba48594fab69a35863884f08fbd788cf552
Yuki Kishimoto <kishimoto.yuki@gmail.com>
2020-03-15 15:25:34 -0400

    Content and styling for MountainDescription component, created data.json to store mountain data

--- client/src/pages/Mountain/Mountain.js
+++ client/src/pages/Mountain/Mountain.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import MountainDescription from "../../components/MountainDescription/MountainDescription";
+
+import data from "../../data/data.json";
+
 import "./Mountain.scss";
 
-export default function Mountain() {
-  return <main className="mountain">Mountain</main>;
+export default function Mountain(props) {
+  const mountainData = data.find(
+    mountain => mountain.url === props.match.params.mountainName
+  );
+
+  return (
+    <main className="mountain">
+      <MountainDescription mountainData={mountainData} />
+    </main>
+  );
 }
