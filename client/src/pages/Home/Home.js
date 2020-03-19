ef7a3da4b160a95fb9fd8083bb9789d216bff444
Roy Lee <royk.leejr@gmail.com>
2020-03-18 21:07:08 -0400

    cleaned commented out code

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,19 +6,6 @@ import SiteDescription from "../../components/SiteDescription/SiteDescription";
 import "./Home.scss";
 
 export default class Home extends React.Component {
-  // componentDidMount() {
-  //   // const header = document.querySelector("header");
-  //   // const home = document.getElementById("home");
-  //   // console.log(home);
-  //   // home.onScroll = this.scrollChange;
-  //   window.addEventListener("scroll", this.handleScroll);
-  // }
-
-  // handleScroll = event => {
-  //   // console.log(event);
-  //   console.log(event.srcElement.body.scrollTop);
-  // };
-
   render() {
     return (
       <main className="home" id="home">
