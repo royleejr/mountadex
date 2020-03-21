246a666597203f3d002e612a6fd6158b067e8079
Roy Lee <royk.leejr@gmail.com>
2020-03-21 19:31:13 -0400

    added hero 3d parallax image and also fixed a bug in header to scroll to top after hitting the about link

--- client/src/pages/Mountain/Mountain.js
+++ client/src/pages/Mountain/Mountain.js
@@ -1,22 +1,20 @@
 import React from "react";
 
 import MountainHeroBlueMountain from "../../components/MountainHeroBlueMountain/MountainHeroBlueMountain";
-import MountainHeroHorseShoeResort from "../../components/MountainHeroHorseShoeResort/MountainHeroHorseShoeResort";
+import MountainHeroHorseshoeResort from "../../components/MountainHeroHorseshoeResort/MountainHeroHorseshoeResort";
 import MountainDescription from "../../components/MountainDescription/MountainDescription";
 import "./Mountain.scss";
 
 export default class Mountain extends React.Component {
   mountain = () => {
     const route = this.props.match.params.mountainName;
+    console.log(route);
     switch (route) {
       case "blue-mountain":
-        return (
-          <>
-            <MountainHeroBlueMountain />
-          </>
-        );
+        return <MountainHeroBlueMountain />;
       case "horseshoe-resort":
-        return <MountainHeroHorseShoeResort />;
+        console.log("this is happening");
+        return <MountainHeroHorseshoeResort />;
       default:
         return <p>No mountain found</p>;
     }
